3590ff0af85ddc4305e5070ad1ce1e257ed51265
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-04 22:06:28 +0400

    change data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
 
+const categories = [
+  { id: 1, title: "Hats" },
+  { id: 2, title: "Jackets" },
+  { id: 3, title: "Sneakers" },
+  { id: 4, title: "Womens" },
+  { id: 5, title: "Mens" },
+];
+
 function App() {
   return (
     <div className="categories-container">
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>shop now</p>
+      {categories.map((item) => (
+        <div className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{item}</h2>
+            <p>Shop now</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
