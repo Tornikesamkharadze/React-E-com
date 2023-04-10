3613d2f7de2075efe739e71f7150c5c6c0415e2a
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-10 19:17:12 +0400

    added links for routing

--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./navigation.style.scss";
+import { ReactComponent as Logo } from "../../assets/shopping-logo-svgrepo-com.svg";
 
 const Navigation = () => {
   return (
     <div className="navigation">
       <Link className="logo-container" to="/">
-        <div>logo</div>
+        <Logo className="logo" />
       </Link>
       <div className="nav-links-container">
-        <Link to="/">Home</Link>
-        <Link to="Shop">Shop</Link>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        <Link className="nav-link" to="Shop">
+          Shop
+        </Link>
+        <Link className="nav-link" to="sign-in">
+          SIGN IN
+        </Link>
       </div>
     </div>
   );
