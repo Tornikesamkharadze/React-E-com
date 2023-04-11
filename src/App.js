2f4488fb9d4e1d8f81dbc4b20c8fd808b57485fb
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-11 23:03:16 +0400

    change routh path

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./routes/home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from "./routes/shop/Shop";
 import Navigation from "./routes/navigation/Navigation";
-import SignIn from "./routes/sign-in/SignIn";
+import Authentication from "./routes/Authentication/Authentication";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route index="/" element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Routes>
     </BrowserRouter>
   );
