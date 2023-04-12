cd1278cc435854377e114cd9bfa3074d178b78bc
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-13 00:12:28 +0400

    remove signoutuser

--- src/contexts/userContext.jsx
+++ src/contexts/userContext.jsx
@@ -1,9 +1,23 @@
-import React, { useContext, useState } from "react";
-
+import React, { useContext, useEffect, useState } from "react";
+import {
+  onAuthStateChangedListener,
+  signOutUser,
+  createUserDocumentFromAuth,
+} from "../utils/firebase/firebase.utils";
 const UserContext = React.createContext();
 
 const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
+    return unsubscribe;
+  }, []);
 
   return (
     <UserContext.Provider value={{ currentUser, setCurrentUser }}>
