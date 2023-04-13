f09ee7814ee1914fd5194d8acc34794af5798ed2
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-14 00:07:17 +0400

    added pruductdataprovider

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { UserProvider } from "./contexts/userContext";
-import { ShopDataProvider } from "./contexts/shopDataContext";
+import { ProductsDataProvider } from "./contexts/productsDataContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserProvider>
-      <ShopDataProvider>
+      <ProductsDataProvider>
         <App />
-      </ShopDataProvider>
+      </ProductsDataProvider>
     </UserProvider>
   </React.StrictMode>
 );
