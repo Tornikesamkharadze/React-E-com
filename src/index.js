c2112e26a11a2b5bf0149aa68cd52a611cddbfe6
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-13 23:54:49 +0400

    added shopdatacontext

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { UserProvider } from "./contexts/userContext";
+import { ShopDataProvider } from "./contexts/shopDataContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserProvider>
-      <App />
+      <ShopDataProvider>
+        <App />
+      </ShopDataProvider>
     </UserProvider>
   </React.StrictMode>
 );
