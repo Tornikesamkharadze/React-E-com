c531bf217babfb41a3d0685797e3366a4334e8f4
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-13 23:47:25 +0400

    added items in shop component

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import SHOP_DATA from "../../data/shop-data.json";
 
 const Shop = () => {
-  return <div>Shop page</div>;
+  return (
+    <div>
+      {SHOP_DATA.map((item) => {
+        const { id, name } = item;
+        return (
+          <div key={id}>
+            <h1>{name}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Shop;
