aa556f6cfc2a96cd3e36fa5d720f62a9495bde79
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-14 23:15:40 +0400

    wrapped app component in cartcontext

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.scss";
 import App from "./App";
 import { UserProvider } from "./contexts/userContext";
 import { ProductsDataProvider } from "./contexts/productsDataContext";
+import { CartContextProvider } from "./contexts/CartContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserProvider>
       <ProductsDataProvider>
-        <App />
+        <CartContextProvider>
+          <App />
+        </CartContextProvider>
       </ProductsDataProvider>
     </UserProvider>
   </React.StrictMode>
