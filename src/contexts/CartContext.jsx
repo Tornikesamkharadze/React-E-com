033bd066ec6175c75199eccc6fae4632603ea209
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-15 00:49:08 +0400

    created addcartitem function. i have to change it later

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,14 +1,34 @@
 import React, { useContext, useState } from "react";
 
-const CartContext = React.createContext();
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
 
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
+const CartContext = React.createContext();
 const CartContextProvider = ({ children }) => {
   const [isCartOpen, setIscardOpen] = useState(false);
-  const modalHendler = () => {
-    setIscardOpen(!isCartOpen);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
   };
+
   return (
-    <CartContext.Provider value={{ isCartOpen, modalHendler }}>
+    <CartContext.Provider
+      value={{ isCartOpen, setIscardOpen, addItemToCart, cartItems }}
+    >
       {children}
     </CartContext.Provider>
   );
