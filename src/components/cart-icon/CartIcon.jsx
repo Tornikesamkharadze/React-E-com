ea4e2ffe84a5909aeed7702de8061cd01b4baae7
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-15 00:45:48 +0400

    added togglehendler function

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -4,9 +4,14 @@ import { ReactComponent as ShopingIcon } from "../../assets/shopping-bag-svgrepo
 import { useCartContext } from "../../contexts/CartContext";
 
 const CartIcon = () => {
-  const { modalHendler } = useCartContext();
+  const { isCartOpen, setIscardOpen } = useCartContext();
+
+  const toggleCartHendler = () => {
+    setIscardOpen(!isCartOpen);
+  };
+
   return (
-    <div className="cart-icon-container" onClick={modalHendler}>
+    <div className="cart-icon-container" onClick={toggleCartHendler}>
       <ShopingIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
