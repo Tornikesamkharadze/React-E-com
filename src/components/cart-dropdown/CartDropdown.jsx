57cdee2408c93fea28993e309ec226736c0fda30
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-15 00:45:06 +0400

    added checkout button

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,11 +1,18 @@
+import CartItem from "../cart-item/CartItem";
+import { useCartContext } from "../../contexts/CartContext";
 import ButtonComponent from "../button/ButtonComponent";
 import "./cart-dropdown.style.scss";
 
 const CartDropDown = () => {
+  const { cartItems } = useCartContext();
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-item" />
-      <ButtonComponent>Go to check out</ButtonComponent>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <ButtonComponent  buttonType="google">CHECKOUT</ButtonComponent>
     </div>
   );
 };
