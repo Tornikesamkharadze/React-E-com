50cd816a814e88afbd6de820961f849867969c42
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-16 21:00:23 +0400

    created removetocard function

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -16,6 +16,22 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
 const CartContext = React.createContext();
 const CartContextProvider = ({ children }) => {
   const [isCartOpen, setIscardOpen] = useState(false);
@@ -33,9 +49,20 @@ const CartContextProvider = ({ children }) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
+  const removeItemToCart = (cartItemToRemove) => {
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
+
   return (
     <CartContext.Provider
-      value={{ isCartOpen, setIscardOpen, addItemToCart, cartItems, cartTotal }}
+      value={{
+        isCartOpen,
+        setIscardOpen,
+        addItemToCart,
+        cartItems,
+        cartTotal,
+        removeItemToCart,
+      }}
     >
       {children}
     </CartContext.Provider>
