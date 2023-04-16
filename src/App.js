4f032876efe8d80894b66df0880ff27558c8b19a
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-16 20:58:02 +0400

    added checkout path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from "./routes/shop/Shop";
 import Navigation from "./routes/navigation/Navigation";
 import Authentication from "./routes/Authentication/Authentication";
+import Checkout from "./routes/checkout/Checkout";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route index="/" element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Routes>
     </BrowserRouter>
   );
