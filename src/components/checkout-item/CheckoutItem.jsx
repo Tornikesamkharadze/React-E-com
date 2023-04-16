421094d865f92e8a8e6efd6b74fcfaff6e5d6266
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-16 23:44:35 +0400

    created checkoutitem component

---
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -0,0 +1,36 @@
+import { useCartContext } from "../../contexts/CartContext";
+import "./checkout-item.styles.scss";
+
+const CheckoutItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  const { addItemToCart, removeItemToCart, filterItemToCart } =
+    useCartContext();
+
+  const filterItem = () => filterItemToCart(cartItem);
+  const addItemHandler = () => addItemToCart(cartItem);
+  const removeItemHandler = () => removeItemToCart(cartItem);
+
+  return (
+    <div className="checkout-item-container">
+      <div className="image-container">
+        <img src={imageUrl} alt={name} />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={removeItemHandler}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={addItemHandler}>
+          &#10095;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <div onClick={filterItem} className="remove-button">
+        &#10005;
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutItem;
