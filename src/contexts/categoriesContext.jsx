b72bb40be741a2f623e58e26521a0794d700b219
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-17 23:52:10 +0400

    created categories context

---
+++ src/contexts/categoriesContext.jsx
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect, useState } from "react";
+import { getCategoriesAndDocumets } from "../utils/firebase/firebase.utils.js";
+import SHOP_DATA from "../data/shop-data.js";
+
+const CategoriesContext = React.createContext();
+
+const CategoriesProvider = ({ children }) => {
+  const [catecogiresMap, setCategoriesMap] = useState([]);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocumets();
+      setCategoriesMap(categoryMap);
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
+  return (
+    <CategoriesContext.Provider value={{ catecogiresMap }}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
+
+const useCategoriesProvider = () => {
+  return useContext(CategoriesContext);
+};
+
+export { CategoriesContext, CategoriesProvider, useCategoriesProvider };
