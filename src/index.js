0f0295f11bb7ae37ffbf579fc33afb93bc52158d
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-17 23:50:06 +0400

    importe categories provider

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
+
 import App from "./App";
 import { UserProvider } from "./contexts/userContext";
-import { ProductsDataProvider } from "./contexts/productsDataContext";
+import { CategoriesProvider } from "./contexts/categoriesContext";
 import { CartContextProvider } from "./contexts/CartContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserProvider>
-      <ProductsDataProvider>
+      <CategoriesProvider>
         <CartContextProvider>
           <App />
         </CartContextProvider>
-      </ProductsDataProvider>
+      </CategoriesProvider>
     </UserProvider>
   </React.StrictMode>
 );
