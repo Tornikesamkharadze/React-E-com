602170e44234463e23c0e00af7496dcd20fad0a1
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-19 01:06:50 +0400

    added useEffect to fix unexpected renders

--- src/routes/category/Category.jsx
+++ src/routes/category/Category.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 import { useCategoriesContext } from "../../contexts/categoriesContext";
+import ProductCard from "../../components/product-card/ProductCard";
 
 import "./category.styles.scss";
-import { useParams } from "react-router-dom";
 
 const Category = () => {
   const { category } = useParams();
   const { catecogiresMap } = useCategoriesContext();
-  return <div>Category</div>;
+  const [products, setProducts] = useState(catecogiresMap[category]);
+
+  useEffect(() => {
+    setProducts(catecogiresMap[category]);
+  }, [category, catecogiresMap]);
+
+  return (
+    <div className="category-container">
+      {products &&
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+    </div>
+  );
 };
 
 export default Category;
