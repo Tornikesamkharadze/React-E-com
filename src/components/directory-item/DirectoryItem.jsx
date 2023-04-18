3be3de77a02819cae035cddf3b23ad3ee934aef7
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-04-19 01:00:20 +0400

    changegd component name

---
+++ src/components/directory-item/DirectoryItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./directoryItem.style.scss";
+
+const DirectoryItem = ({ item }) => {
+  const { imageUrl, title } = item;
+  return (
+    <div className="directory-item-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="body">
+        <h2>{title}</h2>
+        <p>Shop now</p>
+      </div>
+    </div>
+  );
+};
+
+
+export default DirectoryItem;
